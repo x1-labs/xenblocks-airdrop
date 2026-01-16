b13babe947cb259b1aef20c85832e4ad5d0699bd
Nicholas Pettas <nick@x1.xyz>
2026-01-16 14:30:24 -0800

    Remove the web app

--- web/src/hooks/useConnection.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { Connection } from '@solana/web3.js';
-import { config } from '@/config';
-
-export function useConnection(): Connection {
-  return useMemo(() => {
-    return new Connection(config.rpcEndpoint, 'confirmed');
-  }, []);
-}
